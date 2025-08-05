3086845348e927ffc3a8ce47cc199f8c0088269f
Yaswanth876 <vsyaswanth008@gmail.com>
2025-08-05 15:17:45 +0530

    model changed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,7 +3,7 @@ from torch.nn.functional import softmax
 import torch
 
 # Load BERT model and tokenizer only once (reuse across calls)
-model_name = "bhadresh-savani/bert-base-go-emotion"
+model_name = "joeddav/distilbert-base-uncased-go-emotions-student"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
